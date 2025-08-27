e4988d15e9b2cd6eae8432a2f96991718c4baf64
fr-informatique <fabian.measson123@gmail.com>
2025-08-27 15:22:09 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -125,7 +125,7 @@ function App() {
         {/* Hero (orange #DE5121) */}
         <Hero onQuoteClick={openQuotePopup} />
         
-        {/* Services intro (jaune #FDD902) */}
+        {/* Services intro (blanc #FFFFFF) */}
         <div className="relative">
           <div className="scroll-animate services-effect">
             <Services />

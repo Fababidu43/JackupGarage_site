080374416e0970db1f536ff60b40625a22e4e803
fr-informatique <fabian.measson123@gmail.com>
2025-08-27 15:23:52 +0200

    Supprimer l'indicateur de scroll

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -134,12 +134,6 @@ const Hero: React.FC<HeroProps> = ({ onQuoteClick }) => {
         </div>
       </div>
       
-      {/* Indicateur de scroll animé */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce-slow">
-        <div className="w-6 h-10 border-2 border-white/30 rounded-full flex justify-center">
-          <div className="w-1 h-3 bg-orange-400 rounded-full mt-2 animate-scroll-indicator"></div>
-        </div>
-      </div>
     </section>
   );
 };
